092a6d2f681c93afd162c6f9ce6ed7babd286469
Douglas Vinicius <douglas04041996@hotmail.com>
2023-10-10 09:04:19 -0300

    first commit

---
+++ src/main/java/br/com/linkzera/todolist/user/UserController.java
@@ -0,0 +1,53 @@
+package br.com.linkzera.todolist.user;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  @Autowired
+  private IUserRepository userRepository;
+
+  @PostMapping("/")
+  public ResponseEntity<Object> create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+    }
+
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+  }
+
+  @GetMapping("/")
+  public ResponseEntity<List<UserModel>> list() {
+    var users = this.userRepository.findAll();
+    return ResponseEntity.status(HttpStatus.OK).body(users);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<UserModel> findById(@PathVariable UUID id) {
+    var user = this.userRepository.findById(id);
+
+    if (user.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+    return ResponseEntity.status(HttpStatus.OK).body(user.get());
+  }
+
+}
