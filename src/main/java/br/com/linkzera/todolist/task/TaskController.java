3de83c8bd1d8226f1b4d36a864c049f7d13a0655
Douglas Vinicius <douglas04041996@hotmail.com>
2023-10-12 11:53:44 -0300

    chore: validate fixes

--- src/main/java/br/com/linkzera/todolist/task/TaskController.java
+++ src/main/java/br/com/linkzera/todolist/task/TaskController.java
@@ -1,6 +1,6 @@
 package br.com.linkzera.todolist.task;
 
-import java.lang.reflect.Field;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.linkzera.todolist.user.IUserRepository;
 import br.com.linkzera.todolist.user.UserModel;
+import br.com.linkzera.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -33,6 +34,15 @@ public class TaskController {
   public ResponseEntity<Object> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
     var task = this.taskRepository.findByTitle(taskModel.getTitle());
     var user = (UserModel) request.getAttribute("user");
+    var currentDate = LocalDateTime.now();
+
+    if (taskModel.getStartAt().isBefore(currentDate) || taskModel.getEndAt().isBefore(currentDate)) {
+      return ResponseEntity.badRequest().body("Data de início/término não pode ser menor que a data atual!");
+    }
+
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.badRequest().body("Data de início não pode ser maior que a data de término!");
+    }
 
     if (task != null) {
       return ResponseEntity.badRequest().body("Tarefa já existe!");
@@ -92,51 +102,19 @@ public class TaskController {
   }
 
   @PatchMapping("/{id}")
-  public ResponseEntity<Object> update(@PathVariable("id") UUID id, @RequestBody TaskModel taskModel,
-      HttpServletRequest request) {
-    var user = (UserModel) request.getAttribute("user");
-    var task = this.taskRepository.findById(id);
-
-    if (task.isEmpty()) {
-      return ResponseEntity.badRequest().body("Tarefa não encontrada!");
-    }
-
-    if (task.get().getUserId().compareTo(user.getId()) != 0) {
-      return ResponseEntity.badRequest().body("Tarefa não pertence ao usuário!");
-    }
-
-    Class<?> taskClass = task.get().getClass();
-
-    for (Field field : taskModel.getClass().getDeclaredFields()) {
-      try {
-        field.setAccessible(true);
-        Object value = field.get(taskModel);
-
-        if (value != null && taskClass.getDeclaredField(field.getName()) != null) {
-          Field taskField = taskClass.getDeclaredField(field.getName());
-          taskField.setAccessible(true);
-          taskField.set(task.get(), value);
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-
-    var taskUpdated = this.taskRepository.save(task.get());
-
-    return ResponseEntity.ok(taskUpdated);
+  public TaskModel update(@PathVariable("id") UUID id, @RequestBody TaskModel taskModel, HttpServletRequest request) {
+    var task = this.taskRepository.findById(id).orElseThrow(null);
+    Utils.copyNonNullProperties(taskModel, task);
+    this.taskRepository.save(task);
+    return task;
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity<Object> delete(@PathVariable("id") UUID id, HttpServletRequest request) {
     var user = (UserModel) request.getAttribute("user");
-    var task = this.taskRepository.findById(id);
+    var task = this.taskRepository.findById(id).orElse(null);
 
-    if (task.isEmpty()) {
-      return ResponseEntity.badRequest().body("Tarefa não encontrada!");
-    }
-
-    if (task.get().getUserId().compareTo(user.getId()) != 0) {
+    if (task.getUserId().compareTo(user.getId()) != 0) {
       return ResponseEntity.badRequest().body("Tarefa não pertence ao usuário!");
     }
 
